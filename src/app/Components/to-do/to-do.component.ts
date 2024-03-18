e5fdeec450ca252b36be2803f70adb324d925e6c
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-03-18 05:38:40 -1200

    a

---
+++ src/app/Components/to-do/to-do.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-to-do',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    FormsModule
+  ],
+  templateUrl: './to-do.component.html',
+  styleUrl: './to-do.component.css'
+})
+export class ToDoComponent {
+  Title:string = " To Do List Application";
+
+  tasks : string[] = [];
+  newTask : string="";
+  isAvalible : boolean = false;
+
+  addTask()
+  {
+    if(this.newTask.trim() !=="")
+    {
+      this.tasks.push(this.newTask);
+      this.newTask="";
+      this.isAvalible = true;
+    }
+    console.log(this.tasks);
+  }
+  EditTask(index : number)
+  {
+    let updateTask = prompt("Edit Task",this.tasks[index]);
+    if(updateTask !== null)
+    {
+      this.tasks[index] = updateTask.trim();
+    }
+  }
+  RemoveTask(index : number)
+  {
+    this.tasks.splice(index,1);
+    this.isAvalible = this.tasks.length > 0 ;
+  }
+}
